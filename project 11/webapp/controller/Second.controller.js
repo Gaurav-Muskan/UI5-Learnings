2b5322441e39350c6874f5e007c652104b052d75
Gaurav Muskan <gaurav.bharti19088@gmail.com>
2024-05-17 12:34:28 +0530

    BindingJSONfile

--- project 11/webapp/controller/Second.controller.js
+++ project 11/webapp/controller/Second.controller.js
@@ -16,23 +16,9 @@ sap.ui.define(
       onGoThird: function () {
         this.getOwnerComponent().getRouter().navTo("Third");
       },
-      onPatternMatched: function () {
-        // var oModel = this.getOwnerComponent().getModel();
-        // var empId = oModel.getProperty("/empid");
-        // var empName = oModel.getProperty("/empname");
-        // var empDesig = oModel.getProperty("/empdesig");
-        // var empSkill = oModel.getProperty("/empskill");
-        // var empEmail = oModel.getProperty("/empemail");
-        // var empPhone = oModel.getProperty("/empphone");
-
-        // this.getView().byId("idIpEmpId").setText(empId);
-        // this.getView().byId("idIpEmpName").setText(empName);
-        // this.getView().byId("idIpEmpDesignation").setText(empDesig);
-        // this.getView().byId("idIpEmpSkill").setText(empSkill);
-        // this.getView().byId("idIpEmpEmail").setText(empEmail);
-        // this.getView().byId("idIpEmpPhone").setText(empPhone);
-        // You can now use these variables as needed
-      },
+      onGoFirst:function(){
+        this.getOwnerComponent().getRouter().navTo("RouteFirst");
+      }
     });
   }
 );
