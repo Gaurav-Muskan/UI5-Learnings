50ee62fed6cf022d333c28f94e81dcdf2c52bc70
Gaurav Muskan <gaurav.bharti19088@gmail.com>
2024-05-22 16:32:09 +0530

    List View Implemented

--- project13/webapp/controller/First.controller.js
+++ project13/webapp/controller/First.controller.js
@@ -28,38 +28,38 @@ sap.ui.define([
             this.getOwnerComponent().getRouter().navTo("Second");
             // Further processing of the collected data
         },
-        onSelRelation: function(oEvent){
-            var selRel=oEvent.getParameter("selectedItem").getText();
-            console.log(selRel);
-        },
-        onSelRelCB: function(oEvent){
-            var selRelcb=oEvent.getParameter("selectedItem").getText();
-            console.log(selRelcb);
-        },
-        onSelLangs: function(oEvent){
-            var aLanItems=oEvent.getParameter("selectedItems");
-            for(var i=0;i<aLanItems.length;i++){
-                console.log(aLanItems[i].getText());
-            }
-        },
-        onSelectGender: function(oEvent){
-            var index=oEvent.getParameter("selectedIndex");
-            if(index===0){
-                console.log("You are Male");
-            }
-            else {
-                console.log("You are Female");
-            }
-        },
-        onSelChb:function(oEvent){
-            var bIsChecked=oEvent.getParameter("selected");
-            if(bIsChecked){
-                console.log("You are married.");
-            }
-            else{
-                console.log("You are still bachelor.");
-            }
-        },
+        // onSelRelation: function(oEvent){
+        //     var selRel=oEvent.getParameter("selectedItem").getText();
+        //     console.log(selRel);
+        // },
+        // onSelRelCB: function(oEvent){
+        //     var selRelcb=oEvent.getParameter("selectedItem").getText();
+        //     console.log(selRelcb);
+        // },
+        // onSelLangs: function(oEvent){
+        //     var aLanItems=oEvent.getParameter("selectedItems");
+        //     for(var i=0;i<aLanItems.length;i++){
+        //         console.log(aLanItems[i].getText());
+        //     }
+        // },
+        // onSelectGender: function(oEvent){
+        //     var index=oEvent.getParameter("selectedIndex");
+        //     if(index===0){
+        //         console.log("You are Male");
+        //     }
+        //     else {
+        //         console.log("You are Female");
+        //     }
+        // },
+        // onSelChb:function(oEvent){
+        //     var bIsChecked=oEvent.getParameter("selected");
+        //     if(bIsChecked){
+        //         console.log("You are married.");
+        //     }
+        //     else{
+        //         console.log("You are still bachelor.");
+        //     }
+        // },
         // onSubmit: function() {
         //     var empId = this.getView().byId("idIpEmpId").getValue();
         //     var empIdRegEx = /^[0-9]+$/;
