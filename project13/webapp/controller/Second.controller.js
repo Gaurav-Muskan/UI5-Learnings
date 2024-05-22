785a6ebbe99df2256c637518c2a1463ae7706e75
Gaurav Muskan <gaurav.bharti19088@gmail.com>
2024-05-22 12:47:17 +0530

    Intalled Setup

---
+++ project13/webapp/controller/Second.controller.js
@@ -0,0 +1,24 @@
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller"],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller) {
+    "use strict";
+
+    return Controller.extend("restinpeace.project1.controller.Second", {
+      onInit: function () {
+        this.getOwnerComponent()
+          .getRouter()
+          .getRoute("Second")
+          .attachPatternMatched(this.onPatternMatched, this);
+      },
+      onGoThird: function () {
+        this.getOwnerComponent().getRouter().navTo("Third");
+      },
+      onGoFirst:function(){
+        this.getOwnerComponent().getRouter().navTo("RouteFirst");
+      }
+    });
+  }
+);
